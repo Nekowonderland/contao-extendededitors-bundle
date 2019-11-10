33046912667705d65a086f836f65e0d9f880052f
Stefan Heimes <stefan_heimes@hotmail.com>
2019-11-11 00:35:39 +0100

    Correct the extension class
    - Add loader for yml files

--- src/DependencyInjection/ExtendedEditorsExtension.php
+++ src/DependencyInjection/ExtendedEditorsExtension.php
@@ -2,8 +2,10 @@
 
 namespace Nekowonderland\ExtendedEditorsBundle\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 /**
  * Class MultiColumnWizardExtension
@@ -16,8 +18,8 @@ class ExtendedEditorsExtension extends Extension
      * @var array
      */
     private $files = [
-        'listener.yml',
-        'services.yml',
+//        'listener.yml',
+//        'services.yml',
     ];
 
     /**
@@ -32,9 +34,18 @@ class ExtendedEditorsExtension extends Extension
      * Loads a specific configuration.
      *
      * @throws \InvalidArgumentException When provided tag is not defined in this extension
+     *
+     * @throws \Exception
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        // TODO: Implement load() method.
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../Resources/config')
+        );
+
+        foreach ($this->files as $file) {
+            $loader->load($file);
+        }
     }
 }
