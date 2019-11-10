888613342ed31b995583b88aa173cc9ef48a4786
Stefan Heimes <stefan_heimes@hotmail.com>
2019-11-11 00:37:17 +0100

    Correct the plugin class
    - Add routing
    - Correct the BundleConfig::create call

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -7,12 +7,16 @@ use Contao\ManagerBundle\ContaoManagerBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use Nekowonderland\ExtendedEditorsBundle\DependencyInjection\ExtendedEditorsExtension;
+use Contao\ManagerPlugin\Routing\RoutingPluginInterface;
+use Nekowonderland\ExtendedEditorsBundle\ExtendedEditorsBundle;
+use Symfony\Component\Config\Loader\LoaderResolverInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Routing\RouteCollection;
 
 /**
  * Class Plugin
  */
-class Plugin implements BundlePluginInterface
+class Plugin implements BundlePluginInterface, RoutingPluginInterface
 {
     /**
      * {@inheritdoc}
@@ -20,18 +24,23 @@ class Plugin implements BundlePluginInterface
     public function getBundles(ParserInterface $parser)
     {
         return [
-            BundleConfig::create(ExtendedEditorsExtension::class)
+            BundleConfig::create(ExtendedEditorsBundle::class)
                         ->setLoadAfter(
                             [
                                 ContaoCoreBundle::class,
                                 ContaoManagerBundle::class,
                             ]
                         )
-                        ->setReplace(
-                            [
-                                'multicolumnwizard',
-                            ]
-                        ),
         ];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getRouteCollection(LoaderResolverInterface $resolver, KernelInterface $kernel)
+    {
+        return $resolver
+            ->resolve(__DIR__ . '/../Resources/config/routing.yml')
+            ->load(__DIR__ . '/../Resources/config/routing.yml');
     }
 }
